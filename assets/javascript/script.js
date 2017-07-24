8a1a0b79dadd9a1f73888cc9b7aabddb96a17af3
Daniel Hernandez <daniel56hdz@gmail.com>
2017-07-24 16:06:05 -0500

    made form into object

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,11 @@
-
+function FormInputs(trainName, destination, trainTime, frequency){
+    this.trainName = trainName;
+    this.destination = destination;
+    this.trainTime = trainTime;
+    this.frequencyInput = frequency;
+}
+$('#trainForm').submit(function(e){
+    e.preventDefault();
+    var data = JSON.stringify( $('#trainForm').serializeArray() );
+    console.log(data);
+});
